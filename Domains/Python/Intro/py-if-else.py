ade92f8cb80ad19cbe5b7fac966f242c849737ed
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-09-27 10:54:44 -0500

    Python If-Else Statements

---
+++ Domains/Python/Intro/py-if-else.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import sys
+
+
+N = int(raw_input().strip())
+
+if N % 2 != 0:
+    print "Weird"
+
+elif N % 2 == 0:
+    if N in range(2,6):
+        print "Not Weird"
+        
+    elif N in range (6,21):
+        print "Weird"
+       
+    elif N > 20:
+        print "Not Weird"
