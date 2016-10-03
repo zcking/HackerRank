3119b219a751a3a60f365ee4afa0511b8484ccf4
Zachary King <kingzach77@gmail.com>
2016-10-03 11:42:58 -0500

    Reverse the order of a linked list - in C++

---
+++ Domains/DataStructures/LinkedLists/reverse_list.cpp
@@ -0,0 +1,19 @@
+/*
+  Reverse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Reverse(Node *head)
+{
+    if (head == NULL || head->next == NULL) return head;
+    
+    Node* n = Reverse(head->next); 
+    head->next->next = head; 
+    head->next = NULL;
+    return n;
+}
