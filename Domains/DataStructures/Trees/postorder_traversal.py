fd25933c62a7f84c6d3c50d5cb5a0b5f8cfd2548
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-05 10:35:14 -0500

    Tree Traversal in python

---
+++ Domains/DataStructures/Trees/postorder_traversal.py
@@ -0,0 +1,14 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def postOrder(root):
+    #Write your code here
+    if not root:
+        return None
+    
+    postOrder(root.left)
+    postOrder(root.right)
+    print root.data,
