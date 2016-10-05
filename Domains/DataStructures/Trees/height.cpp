c278c6bfb5e84a92350b971827c824cba4f00324
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-05 13:49:52 -0500

    Find the height of a binary tree- Cpp solution

---
+++ Domains/DataStructures/Trees/height.cpp
@@ -0,0 +1,20 @@
+
+/*The tree node has data, left child and right child 
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+};
+
+*/
+int height(node * root)
+{
+  if(root == NULL )
+      return -1;
+
+    
+  return max(height(root->left), height(root->right)) + 1;
+      
+  
+}
